96f298156b254554171f27013a3ff58db9a00049
nenahui <kanatraccoon@gmail.com>
2024-06-14 03:40:15 +0600

    Prepared the basic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { MovieForm } from './components/MovieForm/MovieForm';
+import { MovieItem } from './components/MovieItem/MovieItem';
+
 export const App = () => {
-  return <>App</>;
+  return (
+    <div className={'container-fluid'}>
+      <MovieForm />
+      <MovieItem />
+      <MovieItem />
+      <MovieItem />
+    </div>
+  );
 };
---
+++ src/components/MovieForm/MovieForm.tsx
@@ -0,0 +1,3 @@
+export const MovieForm = () => {
+  return <>MovieForm</>;
+};
